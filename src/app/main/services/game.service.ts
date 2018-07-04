0e582bfafef32dab583b185f76756f04474af5d6
cesarvega <cesarvega.col@gmail.com>
2018-07-04 01:50:09 -0400

    new working copy, ready for data capture

--- src/app/main/services/game.service.ts
+++ src/app/main/services/game.service.ts
@@ -6,26 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GameService {
-  
-  private uri = 'https://nodeapp-ngcclufnxt.now.sh/games/add';
-  // private uri = 'https://nodeappcesar.herokuapp.com/games/add';
+  private uri = 'https://nodeapp-isyasmvajh.now.sh/games/add';
+  // private uri = ' https://nodeapp-tpzbzfapmz.now.sh/games/add';
   constructor(private http: HttpClient) { }
-
-  addGame(name , price): void {
-    const obj =  {
-      name: name,
-      price: price
+  addGame(name, price): void {
+    const obj = {
+      name: 'name',
+      price: 'price'
     };
-    // const headers = new HttpHeaders();
-    // headers.append('Access-Control-Allow-Headers', 'Content-Type');
-    // headers.append('Access-Control-Allow-Methods', 'GET');
-    // headers.append('Access-Control-Allow-Origin', '*');
     this.http.post(this.uri, obj).subscribe(res => console.log('Done'));
   }
-
   getGames(): Observable<Object> {
     return this
-           .http
-           .get(`${this.uri}/games`);
-}
+      .http
+      .get(`${this.uri}/games`);
+  }
 }
