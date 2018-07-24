574d2c4ffb06c2f5ca4e274ffce67c1668ee4a9e
cesarvega <cesarvega.col@gmail.com>
2018-07-24 09:17:17 -0400

    update code at work

--- src/app/main/landing/landing.component.ts
+++ src/app/main/landing/landing.component.ts
@@ -8,6 +8,7 @@ import { locale as english } from './i18n/en';
 import { locale as turkish } from './i18n/tr';
 import { FuseConfigService } from '@fuse/services/config.service';
 import { GameService } from '../services/game.service';
+import { AppointmentService } from '../services/appointments.service';
 @Component({
   selector: 'landing',
   templateUrl: './landing.component.html',
@@ -16,19 +17,13 @@ import { GameService } from '../services/game.service';
 export class LandingComponent implements OnInit, OnDestroy {
   form: FormGroup;
   formErrors: any;
-
-  // Horizontal Stepper
-  horizontalStepperStep1: FormGroup;
-  horizontalStepperStep2: FormGroup;
-  horizontalStepperStep3: FormGroup;
-  horizontalStepperStep1Errors: any;
-  horizontalStepperStep2Errors: any;
-  horizontalStepperStep3Errors: any;
-
+  person;
   // Vertical Stepper
+  verticalStepperStep0: FormGroup;
   verticalStepperStep1: FormGroup;
   verticalStepperStep2: FormGroup;
   verticalStepperStep3: FormGroup;
+  verticalStepperStep0Errors: any;
   verticalStepperStep1Errors: any;
   verticalStepperStep2Errors: any;
   verticalStepperStep3Errors: any;
@@ -38,20 +33,17 @@ export class LandingComponent implements OnInit, OnDestroy {
    * @param {FuseTranslationLoaderService} _fuseTranslationLoaderService
    * @param {FormBuilder} _formBuilder
    */
-
   config: any;
-
   // Private
   private _unsubscribeAll: Subject<any>;
-
-
   constructor(
     private gameservice: GameService,
+    private AppointmentService: AppointmentService,
     private _fuseConfigService: FuseConfigService,
     private _formBuilder: FormBuilder,
     private _fuseTranslationLoaderService: FuseTranslationLoaderService
   ) {
-
+    // this.person = person;
     this._fuseTranslationLoaderService.loadTranslations(english, turkish);
     this._fuseConfigService.config = {
       layout: {
@@ -78,6 +70,7 @@ export class LandingComponent implements OnInit, OnDestroy {
     };
     // Reactive form errors
     this.formErrors = {
+      zipcode: {},
       company: {},
       firstName: {},
       lastName: {},
@@ -89,30 +82,27 @@ export class LandingComponent implements OnInit, OnDestroy {
       country: {}
     };
 
-    // Horizontal Stepper form error
-    this.horizontalStepperStep1Errors = {
+
+    // Vertical Stepper form error
+    this.verticalStepperStep0Errors = {
+      zipcode: {},
       firstName: {},
-      lastName: {}
-    };
-
-    this.horizontalStepperStep2Errors = {
-      address: {}
-    };
-
-    this.horizontalStepperStep3Errors = {
+      lastName: {},
+      address: {},
+      address2: {},
       city: {},
       state: {},
       postalCode: {}
     };
 
-    // Vertical Stepper form error
     this.verticalStepperStep1Errors = {
       firstName: {},
       lastName: {}
     };
 
     this.verticalStepperStep2Errors = {
-      address: {}
+      address: {},
+      address2: {}
     };
 
     this.verticalStepperStep3Errors = {
@@ -124,80 +114,24 @@ export class LandingComponent implements OnInit, OnDestroy {
     // Set the private defaults
     this._unsubscribeAll = new Subject();
   }
- 
-  
+
+
 
   ngOnInit(): void {
-    this._fuseConfigService.config
-      .subscribe((config) => {
-        this.config = config;
-      });
-    // Reactive Form
-    this.form = this._formBuilder.group({
-      company: [
-        {
-          value: 'Google',
-          disabled: true
-        }, Validators.required
-      ],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      address: ['', Validators.required],
-      address2: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      postalCode: ['', [Validators.required, Validators.maxLength(5)]],
-      country: ['', Validators.required]
-    });
-
-    this.form.valueChanges
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe(() => {
-        this.onFormValuesChanged();
-      });
-
-    // Horizontal Stepper form steps
-    this.horizontalStepperStep1 = this._formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required]
-    });
-
-    this.horizontalStepperStep2 = this._formBuilder.group({
-      address: ['', Validators.required]
-    });
-
-    this.horizontalStepperStep3 = this._formBuilder.group({
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      postalCode: ['', [Validators.required, Validators.maxLength(5)]]
-    });
-
-    this.horizontalStepperStep1.valueChanges
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe(() => {
-        this.onFormValuesChanged();
-      });
-
-    this.horizontalStepperStep2.valueChanges
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe(() => {
-        this.onFormValuesChanged();
-      });
-
-    this.horizontalStepperStep3.valueChanges
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe(() => {
-        this.onFormValuesChanged();
-      });
 
     // Vertical Stepper form stepper
+    this.verticalStepperStep0 = this._formBuilder.group({
+      zipcode: ['', Validators.required]
+    });
+
     this.verticalStepperStep1 = this._formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required]
     });
 
     this.verticalStepperStep2 = this._formBuilder.group({
-      address: ['', Validators.required]
+      address: ['', Validators.required],
+      address2: ['', Validators.required]
     });
 
     this.verticalStepperStep3 = this._formBuilder.group({
@@ -205,6 +139,12 @@ export class LandingComponent implements OnInit, OnDestroy {
       state: ['', Validators.required],
       postalCode: ['', [Validators.required, Validators.maxLength(5)]]
     });
+
+    this.verticalStepperStep0.valueChanges
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(() => {
+        this.onFormValuesChanged();
+      });
 
     this.verticalStepperStep1.valueChanges
       .pipe(takeUntil(this._unsubscribeAll))
@@ -226,52 +166,43 @@ export class LandingComponent implements OnInit, OnDestroy {
   }
 
 
-  /**
-   * On destroy
-   */
   ngOnDestroy(): void {
     // Unsubscribe from all subscriptions
     this._unsubscribeAll.next();
     this._unsubscribeAll.complete();
   }
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Public methods
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * On form values changed
-   */
   onFormValuesChanged(): void {
     for (const field in this.formErrors) {
       if (!this.formErrors.hasOwnProperty(field)) {
         continue;
       }
-
       // Clear previous errors
       this.formErrors[field] = {};
-
       // Get the control
       const control = this.form.get(field);
-
       if (control && control.dirty && !control.valid) {
         this.formErrors[field] = control.errors;
       }
     }
   }
 
-  /**
-   * Finish the horizontal stepper
-   */
-  finishHorizontalStepper(): void {
-    alert('You have finished the horizontal stepper!');
-  }
 
   /**
    * Finish the vertical stepper
    */
   finishVerticalStepper(): void {
-    alert('You have finished the vertical stepper!');
+    var jsons = new Array();
+    jsons.push(this.verticalStepperStep0.value);
+    jsons.push(this.verticalStepperStep1.value);
+    jsons.push(this.verticalStepperStep2.value);
+    jsons.push(this.verticalStepperStep3.value);
+    this.AppointmentService.setAppointments(jsons.map(x=>{return Object.values(x)}).join());
+  }
+
+
+  setAppointment(name): void {
+    this.AppointmentService.setAppointments(name);
   }
 
   addGame(name, price): void {
@@ -284,3 +215,40 @@ export class LandingComponent implements OnInit, OnDestroy {
 
 
 }
+
+
+// export const person = {
+//   name: {
+//     label: 'Name',
+//     value: 'Juri',
+//     type: 'text',
+//     validation: {
+//       required: true
+//     }
+//   },
+//   age: {
+//     label: 'Age',
+//     value: 32,
+//     type: 'text'
+//   },
+//   gender: {
+//     label: 'Gender',
+//     value: 'M',
+//     type: 'radio',
+//     options: [{ label: 'Male', value: 'M' }, { label: 'Female', value: 'F' }]
+//   },
+//   city: {
+//     label: 'City',
+//     value: '39010',
+//     type: 'select',
+//     options: [
+//       { label: '(choose one)', value: '' },
+//       { label: 'Bolzano', value: '39100' },
+//       { label: 'Meltina', value: '39010' },
+//       { label: 'Appiano', value: '39057' }
+//     ],
+//     validation: {
+//       required: true
+//     }
+//   }
+// };
--- src/app/main/landing/landing.module.ts
+++ src/app/main/landing/landing.module.ts
@@ -7,7 +7,9 @@ import { FuseSharedModule } from '@fuse/shared.module';
 import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatStepperModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GameService } from '../services/game.service';
-
+import { DynamicFormComponent } from './questions/question-base/dynamic-form.component';
+import { AppointmentService } from '../services/appointments.service';
+import { CalendarModule } from '../apps/calendar/calendar.module';
 
 const routes = [
   {
@@ -31,14 +33,17 @@ const routes = [
     MatStepperModule,
 
     FuseSharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // CalendarModule
+
   ],
   declarations: [
-    LandingComponent
+    LandingComponent,
+    DynamicFormComponent
   ],
   exports     : [
     LandingComponent
   ],
-  providers: [ GameService ]
+  providers: [ GameService, AppointmentService ]
 })
 export class LandingModule { }
